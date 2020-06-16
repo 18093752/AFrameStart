dd47880c486a9bf8c0fbdc555321ace9ed37535b
18093752 <49063992+18093752@users.noreply.github.com>
2020-06-16 07:01:32 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,13 +5,19 @@ var rotationSpeed = -0.005;
 var myOtherBox = document.getElementById('myOtherBox');
 var positionx = 0
 
+x = 0  // center
+y = 0   // center
+r = 5   // radius
+a = 0    // angle (from 0 to Math.PI * 2)
+
+
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed / 2;
 	myOtherBox.object3D.rotation.z += rotationSpeed / 4;
-
-	positionx += rotationSpeed;
-	myOtherBox.object3D.position.x = positionx % 10;
+	a = (a + rotationSpeed) % (Math.PI*2);
+	myOtherBox.object3D.position.x = r * Math.cos(a);
+	myOtherBox.object3D.position.y = r * Math.sin(a);
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
